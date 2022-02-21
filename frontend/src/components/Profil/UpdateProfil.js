a4dc77e2afaa70058b0e4f53a1b040e11390be2c
Mr-S1lence <samsond.co@free.fr>
2022-02-21 17:49:05 +0100

    updload bio frontend

--- frontend/src/components/Profil/UpdateProfil.js
+++ frontend/src/components/Profil/UpdateProfil.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LeftNav from '../leftNav';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import UploadImg from './uploadImg';
+import { updateBio } from '../../actions/user.actions';
 
 const UpdateProfil = () => {
+    const [bio, setBio] = useState('');
+    const [updateForm, setUpdateForm] = useState(false);
     const userData = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch();
+
+    const handleUpdate = () => {
+        dispatch(updateBio(userData._id, bio))
+        setUpdateForm(false);
+    }
 
     return (
         <div>
@@ -17,7 +26,24 @@ const UpdateProfil = () => {
                         <h3>Photo de profil</h3>
                         <img src={userData.picture} alt="avatar utilisateur"/>
                         <UploadImg />
-
+                    </div>
+                    <div className='right-part'>
+                        <div className='bio-update'>
+                            <h3>Bio</h3>
+                            {updateForm === false && (
+                                <>
+                                    <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
+                                    <button onClick={() => setUpdateForm(!updateForm)}>Modifier ma bio</button>
+                                </>
+                            )}
+                            {updateForm && (
+                                <>
+                                    <textarea type="text" defaultValue={userData.bio} onChange={(e) => setBio(e.target.value)}></textarea>
+                                    <button onClick={handleUpdate}>Valider modification</button>
+                                </>
+                            )}
+                        </div>
+                        
                     </div>
                 </div>
             </div>
