50aecaefc92ff480f4992714dc53b652892f4c38
Mr-S1lence <samsond.co@free.fr>
2022-02-25 04:32:32 +0100

    Post formulaire avec prévisualisation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,49 +2,57 @@ import React, { useContext } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { UidContext } from "./AppContext";
-import logo from './../assets/logo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import logo from "./../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import Logout from "./Log/logout";
 
-
 const Navbar = () => {
-    const uid = useContext(UidContext);
-    const userData = useSelector((state) => state.userReducer);
+  const uid = useContext(UidContext);
+  const userData = useSelector((state) => state.userReducer);
 
-    return (
-        <nav>
-            <div className="nav-container">
-                <div className="logo">
-                    <NavLink to ="/">
-                        <div className="logo">
-                            <img src={ logo } alt="icon" />
-                        </div>
-                    </NavLink>
-                </div>
-                {uid ? (
-                    <ul>
-                        <li></li>
-                        <li className="welcome">
-                            <NavLink to="/profil">
-                                <h5>Bienvenue {userData.pseudo}</h5>
-                            </NavLink>
-                        </li>
-                        <Logout />
-                    </ul>
-                 ) : (
-                     <ul>
-                         <li></li>
-                         <li>
-                             <NavLink to="/profil">
-                             <FontAwesomeIcon icon={ faArrowRightToBracket}  size="lg" color="#FD2D01"/>
-                             </NavLink>
-                         </li>
-                     </ul>
-                 )}
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo">
+          <NavLink to="/">
+            <div className="logo">
+              <img src={logo} alt="icon" />
             </div>
-        </nav>
-    )
-}
+          </NavLink>
+        </div>
+        {uid ? (
+          <ul>
+            <li></li>
+            <li className="user-img">
+              <NavLink to="/profil">
+                <img src={userData.picture} alt="user-img" />
+              </NavLink>
+            </li>
+            <li className="welcome">
+              <NavLink to="/profil">
+                <h5>Bienvenue {userData.pseudo}</h5>
+              </NavLink>
+            </li>
+            <Logout />
+          </ul>
+        ) : (
+          <ul>
+            <li></li>
+            <li>
+              <NavLink to="/profil">
+                <FontAwesomeIcon
+                  icon={faArrowRightToBracket}
+                  size="lg"
+                  color="#FD2D01"
+                />
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
---
+++ frontend/src/components/Post/NewPostForm.js
@@ -0,0 +1,109 @@
+import { faImage, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { isEmpty, timestampParser } from "../Utils";
+
+const NewPostForm = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState("");
+  const [postPicture, setPostPicture] = useState(null);
+  const [video, setVideo] = useState("");
+  const [file, setFile] = useState();
+  const userData = useSelector((state) => state.userReducer);
+
+  const handlePicture = () => {};
+
+  const handlePost = () => {};
+
+  const cancelPost = () => {
+    setMessage("");
+    setPostPicture("");
+    setVideo("");
+    setFile("");
+  };
+
+  useEffect(() => {
+    if (!isEmpty(userData)) setIsLoading(false);
+  }, [userData]);
+
+  return (
+    <div className="post-container">
+      {isLoading ? (
+        <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+      ) : (
+        <>
+          <div className="post-form">
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Quoi de neuf ?"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+            {message || postPicture || video.length > 20 ? (
+              <li className="card-container">
+                <div className="card-left">
+                  <img src={userData.picture} alt="user-pic" />
+                </div>
+                <div className="card-right">
+                  <div className="card-header">
+                    <div className="pseudo">
+                      <h3>{userData.pseudo}</h3>
+                    </div>
+                    <span>{timestampParser(Date.now())}</span>
+                  </div>
+                  <div className="content">
+                    <p>{message}</p>
+                    <img src={postPicture} alt="" />
+                    {video && (
+                      <iframe
+                        src={video}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title={video}
+                      ></iframe>
+                    )}
+                  </div>
+                </div>
+              </li>
+            ) : null}
+            <div className="footer-form">
+              <div className="icon">
+                {isEmpty(video) && (
+                  <>
+                    <FontAwesomeIcon icon={faImage} size="lg" color="#FD2D01" />
+                    <input
+                      type="file"
+                      id="file-upload"
+                      name="file"
+                      accept=".jpg, .jpeg, .png"
+                      onChange={(e) => handlePicture(e)}
+                    />
+                  </>
+                )}
+                {video && (
+                  <button onClick={() => setVideo("")}>Supprimer vidéo</button>
+                )}
+              </div>
+              <div className="btn-send">
+                {message || postPicture || video.length > 0 ? (
+                  <button className="cancel" onClick={cancelPost}>
+                    Annuler
+                  </button>
+                ) : null}
+
+                <button className="send" onClick={handlePost}>
+                  Envoyer
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default NewPostForm;
