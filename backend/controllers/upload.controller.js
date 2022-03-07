e13c296bc265d75990291508b0659e538875cd46
Mr-S1lence <samsond.co@free.fr>
2022-03-07 13:11:42 +0100

    upload image profil

---
+++ backend/controllers/upload.controller.js
@@ -0,0 +1,53 @@
+const database = require("../config/db");
+const db = database.getDB();
+const fs = require("fs");
+const { promisify } = require("util");
+const { uploadErrors } = require("../utils/errors.utils");
+const pipeline = promisify(require("stream").pipeline);
+
+module.exports.uploadProfil = async (req, res) => {
+  console.log("uploadt avatar");
+  console.log(req.body);
+  try {
+    if (
+      req.file.detectedMimeType != "image/jpg" &&
+      req.file.detectedMimeType != "image/png" &&
+      req.file.detectedMimeType != "image/jpeg"
+    )
+      throw Error("invalid file");
+
+    if (req.file.size > 500000) throw Error("max size");
+  } catch (err) {
+    const errors = uploadErrors(err);
+    return res.status(201).json({ errors });
+  }
+  const fileName = req.body.name + Date.now() + ".jpg";
+
+  await pipeline(
+    req.file.stream,
+    fs.createWriteStream(
+      `${__dirname}/../../frontend/public/uploads/profil/${fileName}`
+    )
+  );
+
+  const picture = req.file !== null ? "./uploads/profil/" + fileName : "";
+
+  try {
+    const sql =
+      "UPDATE users SET picture ='" +
+      picture +
+      "', updatedAt = NOW() WHERE user_id ='" +
+      req.body.userId +
+      "';";
+    console.log(sql);
+    db.query(sql, async (err, result) => {
+      if (err == null) {
+        res.json(result);
+      } else {
+        console.log(err);
+      }
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
